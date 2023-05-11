bc8fec06cfb26c542c003cae024f1a012befb3b0
dbsminan <dbsminan@hanyang.ac.kr>
2023-05-11 09:58:05 +0900

    total_plot file, New_Folder file

---
+++ src/New_Folder.py
@@ -0,0 +1,13 @@
+import os
+from datetime import datetime
+
+now = datetime.now()
+
+def NewFolder(path):
+    try:
+        if not os.path.exists(path):
+            os.mkdir(path)
+    except OSError:
+        print('Error : Creating Directory '+path)
+
+
