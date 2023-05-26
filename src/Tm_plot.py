685151184b1aaa4eda3b02e057514c7b449f1648
dbsminan <dbsminan@hanyang.ac.kr>
2023-05-26 14:32:41 +0900

    rsq, V값 위치 변경//flat tm 그래프 1550nm,1310nm 부근 확대 그래프 추가

--- src/Tm_plot.py
+++ src/Tm_plot.py
@@ -6,8 +6,8 @@ from lmfit import Model
 import math
 
 def tm_plot(x):
-    xml_file = etree.parse(x)           # load xml file
-    root = xml_file.getroot()           # get root(element) of file
+    xml_file = etree.parse(x)
+    root = xml_file.getroot()
 
 
 
@@ -36,7 +36,7 @@ def tm_plot(x):
             plt.title('Transmission spectra - as measured', fontdict=font_title)
             plt.xlabel('Wavelength[nm]', fontdict=font_axis)
             plt.ylabel('Measured transmission[dB]', fontdict=font_axis)
-            plt.legend(loc='lower center', ncol=2, fontsize=10)
+            plt.legend(loc='lower center', ncol=2, fontsize=6)
 
         # Reference
         elif i.tag == 'Modulator':
@@ -44,8 +44,8 @@ def tm_plot(x):
                 wl_ref = list(map(float, i.find('PortCombo').find('WavelengthSweep').find('L').text.split(',')))
                 tm_ref = list(map(float, i.find('PortCombo').find('WavelengthSweep').find('IL').text.split(',')))
                 plt.plot(wl_ref, tm_ref, color='#7f7f7f', linestyle=':', label='Reference')
-                plt.subplot(2, 3, 2)
-                plt.plot(wl_ref, tm_ref, color='#7f7f7f', linestyle=':', label='Reference')
+
+                # plt.plot(wl_ref, tm_ref, color='#7f7f7f', linestyle=':', label='Reference')
 
     # Wavelength-Transmission(Fitting)
     rsq_ref = []
@@ -53,30 +53,15 @@ def tm_plot(x):
         fit = np.polyfit(np.array(wl_ref), np.array(tm_ref), p)
         fit_eq = np.poly1d(fit)
         rsq_ref.append(r2_score(tm_ref, fit_eq(wl_ref)))
-        plt.plot(wl_ref, fit_eq(wl_ref), label=f'{p}th R² : {r2_score(tm_ref, fit_eq(wl_ref))}')
+        # plt.plot(wl_ref, fit_eq(wl_ref), label=f'{p}th R² : {r2_score(tm_ref, fit_eq(wl_ref))}')
 
-    plt.title('Transmission spectra - as measured', fontdict=font_title)
-    plt.xlabel('Wavelength[nm]', fontdict=font_axis)
-    plt.ylabel('Measured transmission[dB]', fontdict=font_axis)
-    plt.legend(loc='lower center', fontsize=8)
-
-    # DC_bias = -2.0
-
-    #
-    # for j in range(6):
-    #     plt.plot(wl_ref, tm_ref - fit_eq(wl_ref))
-    #     plt.plot(wl_list[j], tm_list[j] - fit_eq(wl_list[j]), plot_color[j], label=f'DC_bias={DC_bias}V')
-    #     DC_bias += 0.5
-    #
-    # plt.title('Flat Transmission spectra - as measured', fontdict=font_title)
+    # plt.title('Transmission spectra - as measured', fontdict=font_title)
     # plt.xlabel('Wavelength[nm]', fontdict=font_axis)
     # plt.ylabel('Measured transmission[dB]', fontdict=font_axis)
-    # plt.legend(loc='lower center', ncol=2, fontsize=10)
+    # plt.legend(loc='lower center', fontsize=8)
 
 
-
-
-    plt.subplot(2, 3, 3)
+    plt.subplot(2, 3, 2)
     DC_bias = -2.0
     linear_x = []
     linear_y = []
@@ -86,32 +71,67 @@ def tm_plot(x):
             if data[i] > max(data[i - 200:i]) and data[i] > max(data[i + 1:i + 200]):
                 maxima_idx.append(i)
         return maxima_idx
+
+    wl_flat_l, tm_flat_l = [], []
     for j in range(6):
         maxidx = find_local_maxima_idx(tm_list[j] - fit_eq(wl_list[j]))
         for i in maxidx:
             linear_x.append(wl_list[j][i])
             linear_y.append(tm_list[j][i] - fit_eq(wl_list[j][i]))
-        # print(linear_x)
-        # print(linear_y)
 
         afc = np.polyfit(linear_x, linear_y, 1)
-        # poly1d 함수를 사용하여 1차 근사 함수를 만듦
 
         af = np.poly1d(afc)
 
-        plt.plot(wl_list[j], tm_list[j] - fit_eq(wl_list[j])- af(wl_list[j]),label=f'DC_bias={DC_bias}V')
-        linear_y=[]
-        linear_x=[]
+        wl_flat = wl_list[j]
+        wl_flat_l.append(wl_flat)
+
+        tm_flat = tm_list[j] - fit_eq(wl_list[j]) - af(wl_list[j])
+        tm_flat_l.append((tm_flat))
+
+        plt.plot(wl_flat, tm_flat, label=f'DC_bias={DC_bias}V')
+
         DC_bias += 0.5
 
     plt.title('Flat Transmission spectra - as measured', fontdict=font_title)
     plt.xlabel('Wavelength[nm]', fontdict=font_axis)
     plt.ylabel('Measured transmission[dB]', fontdict=font_axis)
-    plt.legend(loc='lower center', ncol=2, fontsize=10)
+    plt.legend(loc='lower center', ncol=2, fontsize=6)
 
+    plt.subplot(2, 3, 3)
+    DC_bias = -2.0
+    for i in range(6):
+        if wl_flat_l[i][0] >= 1500:
+            wl_a, tm_a = [], []
+            for a in wl_flat_l[i]:
+                if (1549 < a < 1551):
+                    wl_a.append(a)
+                    wl_index = wl_flat_l[i].index(wl_a[0])
 
+            for b in tm_flat_l[i][wl_index:(wl_index + len(wl_a))]:
+                tm_a.append(b)
 
+            plt.plot(wl_a, tm_a, ':', label=f'DC_bias={DC_bias}V')
+            DC_bias += 0.5
 
+        else:
+            wl_a, tm_a = [], []
+            for a in wl_flat_l[i]:
+                if (1309 < a < 1311):
+                    wl_a.append(a)
+                    wl_index = wl_flat_l[i].index(wl_a[0])
+
+            for b in tm_flat_l[i][wl_index:(wl_index + len(wl_a))]:
+                tm_a.append(b)
+
+            plt.plot(wl_a, tm_a, ':', label=f'DC_bias={DC_bias}V')
+            DC_bias += 0.5
+
+    plt.title('Flat Transmission spectra - as measured', fontdict=font_title)
+    plt.xlabel('Wavelength[nm]', fontdict=font_axis)
+    plt.ylabel('Measured transmission[dB]', fontdict=font_axis)
+    plt.xticks(np.arange(1549, 1551.1, 0.5))
+    plt.legend(loc='upper right', ncol=2, fontsize=6)
 
 
     plt.subplot(2, 3, 5)
@@ -122,7 +142,7 @@ def tm_plot(x):
         return I_0 * np.array(list(map(math.sin, pi * delta_l * n_eff / wave_length / 10 ** (-9)))) ** 2
     model = Model(fitting_find_n_eff, independent_vars=["wave_length"],param_names=['n_eff'])
     model.set_param_hint('n_eff',value=4.2,min=0.0,max=10.0)
-        # 초기 매개 변수 설정
+
 
 
     linear_x = []
@@ -136,11 +156,10 @@ def tm_plot(x):
     for i in maxidx:
         linear_x.append(wl_list[v0ind][i])
         linear_y.append(tm_list[v0ind][i] - fit_eq(wl_list[v0ind][i]))
-        # print(linear_x)
-        # print(linear_y)
+
 
     afc = np.polyfit(linear_x, linear_y, 1)
-        # poly1d 함수를 사용하여 1차 근사 함수를 만듦
+
 
     af = np.poly1d(afc)
     flat_tm_list = [tm - fit_eq(wl) - af(wl) for wl, tm in zip(wl_list[v0ind], tm_list[v0ind])]
@@ -151,7 +170,7 @@ def tm_plot(x):
 
     result = model.fit(linear_tm, wave_length=wl_list[v0ind])
     n_eff_value = result.best_values['n_eff']
-    # print(n_eff_value)
+
 
     bias=[-2.0,-1.5,-1.0,-0.5,0.0,0.5]
     linear_x = []
@@ -164,7 +183,7 @@ def tm_plot(x):
             delta_l = 40 * 10 ** (-6)
             l = 500 * 10 ** (-6)
             V=bias[j]
-            # n_eff = n_eff_value
+
             pi = math.pi
             return I_0 * np.array(list(map(math.sin, pi * delta_l * n_eff / wave_length / 10 ** (-9))) + pi * l * V * delta_n_eff / wave_length / 10 ** (-9)) ** 2
 
@@ -175,11 +194,10 @@ def tm_plot(x):
         for t in maxidx:
             linear_x.append(wl_list[j][t])
             linear_y.append(tm_list[j][t] - fit_eq(wl_list[j][t]))
-            # print(linear_x)
-            # print(linear_y)
+
 
         afc = np.polyfit(linear_x, linear_y, 1)
-            # poly1d 함수를 사용하여 1차 근사 함수를 만듦
+
 
         af = np.poly1d(afc)
         flat_tm_list = [tm - fit_eq(wl) - af(wl) for wl, tm in zip(wl_list[j], tm_list[j])]
@@ -194,10 +212,10 @@ def tm_plot(x):
 
         delta_n_eff_list.append(delta_n_eff_value)
 
-
         linear_y = []
         linear_x = []
         linear_tm = []
+
     plt.title('Flat-Flat linear Transmission spectra', fontdict=font_title)
     plt.xlabel('wavelength [nm]', fontdict=font_axis)
     plt.ylabel('intensity', fontdict=font_axis)
